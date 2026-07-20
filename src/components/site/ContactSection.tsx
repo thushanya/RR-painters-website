cdd80e6f857d62749c840d3d36e293ea52814892
Thushanya <thushanyajeyathasan@gmail.com>
2026-07-20 11:55:05 +0530

    Update contact Quote email

--- src/components/site/ContactSection.tsx
+++ src/components/site/ContactSection.tsx
@@ -14,7 +14,7 @@ export function ContactSection() {
     const data = new FormData(form);
 
     try {
-      const response = await fetch("https://formspree.io/f/mdarqrzw", {
+      const response = await fetch("https://formspree.io/f/mpqvrpkp", {
         method: "POST",
         body: data,
         headers: {
